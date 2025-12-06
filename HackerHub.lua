f4cb1e2b99bf89dfcd6958cec0ca4b739c28ea80
Theus999 <mateuspedrosaoliveira472000@gmail.com>
2025-12-06 12:36:04 -0300

    update HackerHub.lua

--- HackerHub.lua
+++ HackerHub.lua
@@ -3304,6 +3304,7 @@ end})
 
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
+
 WindUI:AddTheme({
     Name = "Dark", -- theme name
     
